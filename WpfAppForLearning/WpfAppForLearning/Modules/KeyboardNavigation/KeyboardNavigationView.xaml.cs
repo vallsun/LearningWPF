4bc77ef5c4c16a0edb97efaa42ce96b8f74cb11e
vallsun <39959183+vallsun@users.noreply.github.com>
2019-01-20 01:55:13 +0900

    F　コンテンツ：KeyBoardNavigation　レイアウトを変更、参考情報を追加

--- WpfAppForLearning/WpfAppForLearning/Modules/KeyboardNavigation/KeyboardNavigationView.xaml.cs
+++ WpfAppForLearning/WpfAppForLearning/Modules/KeyboardNavigation/KeyboardNavigationView.xaml.cs
@@ -41,5 +41,12 @@ namespace WpfAppForLearning.Modules.KeyboardNavigation
             }
             
         }
+
+        private void hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
+
+        }
     }
 }
