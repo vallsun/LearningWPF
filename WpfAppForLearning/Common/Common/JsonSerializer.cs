0d6f4d354d68d07fff74a50c5cf3515a56b0207d
kamiya-s <shohei.kamiya.j2n@jpgr.denso.com>
2023-09-24 21:02:31 +0900

    F Jsonシリアライズのテストドライバを追加

---
+++ WpfAppForLearning/Common/Common/JsonSerializer.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace DevelopmentCommon.Common
+{
+	public class JsonSerializer
+	{
+		#region 内部フィールド
+
+		private Encoding m_DefaultEncoding = new UTF8Encoding(false);
+
+		#endregion
+
+		#region 公開サービス
+
+		public void Save<T>(string folder, string fileName, T target)
+		{
+			if(!Directory.Exists(folder))
+			{
+				Directory.CreateDirectory(folder);
+			}
+
+			string filePath = folder + "\\" + fileName;
+
+			var options = new JsonSerializerOptions
+			{
+				// JavaScriptEncoder.Createでエンコードしない文字を指定するのも可
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+				// 読みやすいようインデントを付ける
+				WriteIndented = true
+			};
+			string jsonData = System.Text.Json.JsonSerializer.Serialize(target, options);
+
+			StreamWriter sw = new StreamWriter(filePath, false, m_DefaultEncoding);
+			try
+			{
+				sw.Write(jsonData);
+			}
+			finally
+			{
+				sw.Close();
+			}
+		}
+
+		#endregion
+
+
+	}
+}
---
+++ WpfAppForLearning/SerializeTestDriver/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using DevelopmentCommon.Common;
+
+namespace SerializeTestDriver
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var serializer = new JsonSerializer();
+			var customers = new Person[]
+{
+				new Person() { Name = "山田", Age = 29 },
+				new Person() { Name = "鈴木", Age = 22 }
+};
+			serializer.Save(@"C:\Users\godva", "Sample.json", customers);
+		}
+
+		private class Person
+		{
+			public string Name { get; set; }
+
+			/// <summary>
+			/// 年齢
+			/// </summary>
+			/// <remarks>
+			/// JsonIgnoreの属性を設定することでシリアライズ対象から外すことができる
+			/// </remarks>
+			[JsonIgnore]
+			public int Age { get; set; }
+			public override string ToString() => $"Name:{Name}, Age:{Age}";
+		}
+	}
+}
